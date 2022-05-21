10538a2729809a71e14fe9ab8f52c2f3e3537b1d
ImAClownAndDontComplain <lukporey1317@bk.ru>
2022-05-21 10:44:28 +0500

    task 25

---
+++ 25/skybox.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "camera.h"
+#include "skybox_technique.h"
+#include "cubemaptexture.h"
+#include "mesh.h"
+
+class SkyBox
+{
+public:
+    SkyBox(const Camera* pCamera, const PersProjInfo& p);
+
+    ~SkyBox();
+
+    bool Init(const string& Directory,
+        const string& PosXFilename,
+        const string& NegXFilename,
+        const string& PosYFilename,
+        const string& NegYFilename,
+        const string& PosZFilename,
+        const string& NegZFilename);
+
+    void Render();
+
+private:
+    SkyboxTechnique* m_pSkyboxTechnique;
+    const Camera* m_pCamera;
+    CubemapTexture* m_pCubemapTex;
+    Mesh* m_pMesh;
+    PersProjInfo m_persProjInfo;
+};
+
