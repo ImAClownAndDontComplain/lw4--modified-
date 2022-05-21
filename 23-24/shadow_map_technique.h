42733f0a2851825dc409e7aab8962513b0deb1c6
ImAClownAndDontComplain <lukporey1317@bk.ru>
2022-05-21 10:55:54 +0500

    23-24

---
+++ 23-24/shadow_map_technique.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "technique.h"
+#include "math_3d.h"
+
+class ShadowMapTechnique : public Technique {
+
+public:
+    ShadowMapTechnique();
+    virtual bool Init();
+    void SetWVP(const Matrix4f& WVP);
+    void SetTextureUnit(unsigned int TextureUnit);
+
+private:
+
+    GLuint m_WVPLocation;
+    GLuint m_textureLocation;
+};
